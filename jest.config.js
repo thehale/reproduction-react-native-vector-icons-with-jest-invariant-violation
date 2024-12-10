e8ed804f0a49bfd3d8398aabe1e705bdefd4157e
Joseph Hale <me@jhale.dev>
2024-12-09 18:56:15 -0700

    NOT A FIX: Try using `moduleNameMapper`
    No effect on the test success/failure

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    "@react-native-vector-icons/common": require.resolve("@react-native-vector-icons/common"),
+    "@react-native-vector-icons/entypo": require.resolve("@react-native-vector-icons/entypo"),
+  },
 };
